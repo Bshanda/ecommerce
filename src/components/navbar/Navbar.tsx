5248b4b16f6e4f92ee9fc9e9ac47d4a8130685a3
Balsher Singh <balshersinghhanda87@gmail.com>
2024-09-18 11:16:40 +0530

    1- Mobile nav menu changed

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -18,8 +18,8 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className="flex justify-between text-xs relative">
-                <h1 className="logo text-2xl font-medium flex  items-center  gap-1 cursor-pointer">
+            <nav className="flex h-10 justify-between text-xs relative">
+                <h1 className="logo text-2xl font-medium flex  items-center  gap-1 cursor-pointer tracking-wide">
 
                     <GiHamburgerMenu className='md:hidden' onClick={() => setMobileNavMenu((prev) => !prev)} />
                     Shop
@@ -71,37 +71,21 @@ import { IoCloseSharp } from "react-icons/io5";
 
 const MobileNavMenu = ({ closeNavMenu }: { closeNavMenu: () => void }
 ) => {
-    const [dropdownVisibility, setDropdownVisibility] = useState<boolean>(false)
-
-    const toggleDropdownVisibility = () => { setDropdownVisibility((prev) => !prev) }
-
     return (
-        <div className='w-[100%] md:hidden border border-black z-10 bg-white'>
+        <div className='w-[100%] md:hidden   z-10 bg-white'>
             <div className='flex justify-end   text-end'>
                 <IoCloseSharp className='text-end rounded-md ' onClick={closeNavMenu} />
 
             </div>
-            <div className='text-xs mb-2'>
-                <ul className='flex flex-col gap-2 text-black items-center '>
-                    <li className='p-1 rounded-md cursor-pointer'>Home</li>
-                    <li className='p-1 rounded-md  cursor-pointer' onClick={toggleDropdownVisibility} >
-                        <div className='flex items-center gap-1'>
-
-                            Shop <FaChevronDown className='animate-pulse' />
-                        </div>
-                        {dropdownVisibility ?
-                            <div className='bg-slate-50'>
-                                <ul className='text-center bg-slate-50'>
-                                    <li className='cursor-pointer border-b hover:border-dashed'>Men</li>
-                                    <li className='cursor-pointer border-b hover:border-dashed'>Women</li>
-                                    <li className='cursor-pointer border-b hover:border-dashed'>Unisex</li>
-                                </ul>
-                            </div>
-                            : null}
+            <div className='text-xs mb-2 h-[calc(100vh - )]'>
+                <ul className='flex flex-col gap-2 text-black items-center text-center '>
+                    <li className='p-1 w-full rounded-md cursor-pointer'>Home</li>
+                    <li className='p-1 w-full rounded-md  cursor-pointer'   >
+                        Shop
                     </li>
-                    <li className='p-1 rounded-md cursor-pointer'>Our Story</li>
-                    <li className='p-1 rounded-md cursor-pointer'>About</li>
-                    <li className='p-1 rounded-md cursor-pointer'>Contact Us</li>
+                    <li className='p-1 w-full rounded-md cursor-pointer'>Our Story</li>
+                    <li className='p-1 w-full rounded-md cursor-pointer'>About</li>
+                    <li className='p-1 w-full rounded-md cursor-pointer'>Contact Us</li>
                 </ul>
             </div>
 
